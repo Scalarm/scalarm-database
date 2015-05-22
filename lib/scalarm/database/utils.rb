bb5038b2bf1877a5b799468a1c542266e69e5a16
Jakub Liput <jakub.liput@gmail.com>
2015-05-22 16:42:03 +0200

    Fixes in Scalarm::Database::Utils module name

--- lib/scalarm/database/utils.rb
+++ lib/scalarm/database/utils.rb
@@ -1,9 +1,11 @@
-module Scalarm::Database::Utils
-  def self.parse_json_if_string(value)
-    value.kind_of?(String) and JSON.parse(value) or value
-  end
-
-  def self.to_bson_if_string(object_id)
-    object_id.kind_of?(String) ? BSON::ObjectId(object_id) : object_id
+module Scalarm::Database
+  module Utils
+    def self.parse_json_if_string(value)
+      value.kind_of?(String) and JSON.parse(value) or value
+    end
+  
+    def self.to_bson_if_string(object_id)
+      object_id.kind_of?(String) ? BSON::ObjectId(object_id) : object_id
+    end
   end
 end
--- lib/scalarm/database/version.rb
+++ lib/scalarm/database/version.rb
@@ -1,5 +1,5 @@
 module Scalarm
   module Database
-    VERSION = '0.1.3.1'
+    VERSION = '0.1.3.2'
   end
 end
