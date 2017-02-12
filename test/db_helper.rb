6efdd80ba4549c119233746e08515424d0f92899
dkrol <dkrol3@gmail.com>
2017-02-12 12:48:20 +0100

    Supporting MongoDB at remote hosts.

--- test/db_helper.rb
+++ test/db_helper.rb
@@ -2,23 +2,33 @@ require 'scalarm/database/core/mongo_active_record'
 
 module DBHelper
 
-  DATABASE_NAME = 'scalarm_db_test'
+  def setup(db_config=nil)
+    db_config ||= Rails.application.secrets.database
+    default_mongodb_host = db_config['host'] || 'localhost'
+    default_mongodb_db_name = db_config['db_name'] || 'scalarm_db_test'
 
-  def setup
-    Scalarm::Database::MongoActiveRecord.set_encryption_key('test_key')
+    puts "MongoDB configuration: #{db_config}"
+    puts "Connecting to database #{default_mongodb_host} --- #{default_mongodb_db_name}"
+
+    Scalarm::Database::MongoActiveRecord.set_encryption_key(db_config['db_secret_key'] || 'test_key')
 
     unless Scalarm::Database::MongoActiveRecord.connected?
-      unless Scalarm::Database::MongoActiveRecord.connection_init('localhost', DATABASE_NAME)
+      unless Scalarm::Database::MongoActiveRecord.connection_init(default_mongodb_host, default_mongodb_db_name)
         raise StandardError.new('Connection to database failed')
       end
-      puts "Connecting to database #{DATABASE_NAME}"
     end
   end
 
   # Drop all collections after each test case.
-  def teardown
-    Scalarm::Database::MongoActiveRecord.connection_init('localhost', DATABASE_NAME)
-    db = Scalarm::Database::MongoActiveRecord.get_database(DATABASE_NAME)
+  def teardowndb_config=nil
+    db_config ||= Rails.application.secrets.database
+    default_mongodb_host = db_config['host'] || 'localhost'
+    default_mongodb_db_name = db_config['db_name'] || 'scalarm_db_test'
+
+    puts db_config
+
+    Scalarm::Database::MongoActiveRecord.connection_init(default_mongodb_host, default_mongodb_db_name)
+    db = Scalarm::Database::MongoActiveRecord.get_database(default_mongodb_db_name)
     db.drop
   end
 end
--- test/integration/connection_init_test.rb
+++ test/integration/connection_init_test.rb
@@ -6,8 +6,13 @@ require 'scalarm/database/core/mongo_active_record'
 class ConnectionInitTest < MiniTest::Test
   def test_localhost_get_collection
     # TODO: drop collection first
-    init_result = Scalarm::Database::MongoActiveRecord.connection_init('localhost', 'scalarm_test_database')
-    assert (init_result == true), "connection: #{init_result.to_s}, is mongo running on localhost?"
+    db_config = Rails.application.secrets.database
+    puts db_config
+    default_mongodb_host = db_config['host'] || 'localhost'
+    default_mongodb_db_name = db_config['db_name'] || 'scalarm_db_test'
+
+    init_result = Scalarm::Database::MongoActiveRecord.connection_init(default_mongodb_host, default_mongodb_db_name)
+    assert (init_result == true), "connection: #{init_result.to_s}, is a mongodb running on #{default_mongodb_host}?"
     collection = Scalarm::Database::MongoActiveRecord.get_collection('test1')
     refute_nil collection
     collection.insert_one(hello: 'world')
